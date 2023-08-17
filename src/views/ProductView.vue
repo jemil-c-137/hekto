bbc070b5affc7c86f0f715d9d8cbb00fa71429a8
jemil-c-137 <jemil1931@gmail.com>
2023-08-17 22:59:51 +0200

    add headline

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,21 +1,25 @@
 <template>
-  <h1>product details</h1>
+  <h1>🚧Page {{ productName }} under construction🚧</h1>
 </template>
 
 <script setup lang="ts">
 import Api from '@/api'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
+
+const productName = ref<string>()
 
 onMounted(() => {
   const id = route.params.id
   console.log(id, 'id')
   if (id) {
-    Api.getProductDetails(+id).then((res) => {
-      console.log(res, 'res')
-    })
+    Api.getProductDetails(+id)
+      .then((res) => {
+        productName.value = res.name
+      })
+      .catch((e) => console.log('error', e))
   }
 })
 </script>
