37b60a57bebaf2b7d30ca0cb6733dd64d817ad36
jemil-c-137 <jemil1931@gmail.com>
2024-12-08 18:21:14 +0100

    Update NewsletterForm.vue: Adjust layout for better responsiveness and update background image URL

--- src/modules/Newsletter/components/NewsletterForm.vue
+++ src/modules/Newsletter/components/NewsletterForm.vue
@@ -1,6 +1,6 @@
 <template>
     <section
-        class="pt-56 pb-32 text-center bg-[url('https://github.com/jemil-c-137/hekto/blob/gh-pages/assets/bg-palm.jpg?raw=true')] bg-no-repeat"
+        class="max-w-screen-2xl mx-auto flex flex-col items-center justify-center mt-24 pt-56 pb-32 bg-[url('https://github.com/jemil-c-137/hekto/blob/gh-pages/assets/bg-palm.jpg?raw=true')] bg-no-repeat"
     >
         <h3 class="mb-7 section-title">Get Leatest Update By Subscribe 0ur Newslater</h3>
         <SignUpField />
