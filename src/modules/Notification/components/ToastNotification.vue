648f0440b97e4bb79a1d97e7c9f5983436b321d3
jemil-c-137 <jemil1931@gmail.com>
2024-12-11 20:25:04 +0100

    Add newsletter subscription API and notification components
    - Introduced `subscribeToNewsletter` API function for handling newsletter subscriptions.
    - Created `ToastNotification` component for displaying alerts.
    - Implemented `alertStore` using Pinia for managing alert messages with success, error, and info types.
    - Exported necessary functionalities from the notification module for better accessibility.

---
+++ src/modules/Notification/components/ToastNotification.vue
@@ -0,0 +1,21 @@
+<template>
+    <Teleport to="body">
+        <div class="fixed top-12 left-1/2 -translate-x-1/2 z-5000">
+            <AlertMessage v-for="alert in alerts"
+                        :message="alert.message"
+                        :type="alert.type"
+                        :key="alert.message" />
+        </div>
+    </Teleport>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+import AlertMessage from '@/UI/AlertMessage.vue'
+import { useAlertStore } from '../store/alertStore';
+
+const alertStore = useAlertStore();
+
+const { alerts } = storeToRefs(alertStore);
+</script>
