6a1a608e86df81ea486a75fa88adbb870d1c1376
jemil-c-137 <jemil1931@gmail.com>
2024-12-30 16:20:19 +0100

    Update product detail layout and add Iconify support
    - Introduced @iconify/vue and @iconify/types as dev dependencies for icon support.
    - Enhanced ProductDetail.vue with new layout features, including share icons and additional product information tabs (Description, Additional info, Reviews, Video).
    - Updated styles in style.scss to include typography imports and improved overall styling.
    - Simplified ProductPage.vue structure for better readability.

--- src/modules/Product/components/product-detail/ProductDetail.vue
+++ src/modules/Product/components/product-detail/ProductDetail.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex bg-white shadow-2xl max-w-7xl p-4 font-josefin">
+    <div class="content-container flex bg-white shadow-2xl max-w-7xl p-4 font-josefin">
         <div class="w-5/12 flex justify-center items-center">
             <img :src="product.imageUrl" alt="">
         </div>
@@ -18,18 +18,84 @@
             </p>
             <div class="mt-3 text-brand-semi">{{ product.description }}</div>
             <BaseButton class="mt-3">Add To Cart</BaseButton>
-            <div class="mt-3">Categories: <BaseBadge v-for="category in product.categories" :key="category" bg-color="brand-interactive" class="mr-1">{{ category }}</BaseBadge></div>
-            <div>Tags: <BaseBadge bg-color="brand-highlight">{{ product.tags }}</BaseBadge></div>
-            <div>Share</div>
+            <div class="mt-3 flex items-center">
+                <p class="mr-1">Categories: </p>
+                <BaseBadge v-for="category in product.categories" :key="category" bg-color="brand-interactive" class="mr-1">{{ category }}</BaseBadge>
+            </div>
+            <div class="mt-2">Tags: <BaseBadge bg-color="brand-highlight">{{ product.tags }}</BaseBadge></div>
+            <div class="mt-2 flex items-center">
+                <p class="mr-1">Share: </p>
+                <Icon icon="bi:instagram" class="text-brand-interactive mr-2" />
+                <Icon icon="bi:twitter-x" class="text-brand-interactive mr-2" />
+                <Icon icon="bi:facebook" class="text-brand-interactive" />
+            </div>
         </div>
+    </div>
+    <div class="bg-brand-bg-primary mt-48 pt-24 mb-48 w-full">
+        <div class="content-container">
+            <ul class="flex text-lg font-semibold gap-48">
+                <li v-for="tab in tabs"
+                    :key="tab"
+                    class="cursor-pointer hover:text-brand-interactive"
+                    :class="[activeTab === tab && 'underline text-brand-primary']"
+                    @click="activeTab = tab">
+                        {{ tab }}
+                </li>
+            </ul>
+
+            <div>
+                <!-- DESCRIPTION -->
+                <div v-if="activeTab === 'Description'" class="mt-12">
+                    
+                    <p>
+                        Experience the perfect blend of elegance and comfort with the Playwood Armchair. Crafted from high-quality materials, this chair combines a sleek design with ergonomic support to enhance your living space. Ideal for modern interiors, it features smooth wood finishes and a cushioned seat for hours of relaxation. Whether you’re reading, working, or entertaining, the Playwood Armchair adapts seamlessly to your lifestyle.
+                    </p>
+                </div>
+
+
+                <!-- ADDITIONAL INFO -->
+                <div v-if="activeTab === 'Additional info'" class="mt-12">
+                    <ul>
+                        <li><strong>Material: </strong> Premium solid wood frame with foam cushioning</li>
+                        <li><strong>Dimensions: </strong> 85cm (H) x 75cm (W) x 70cm (D)</li>
+                        <li><strong>Weight Capacity: </strong> Up to 120kg</li> 
+                        <li><strong>Color Options: </strong> Natural Oak, Walnut, Black Finish</li>
+                        <li><strong>Assembly: </strong> Partial assembly required (tools included)</li> 
+                        <li><strong>Care Instructions: </strong> Clean with a damp cloth; avoid abrasive cleaners</li>
+
+                    </ul>
+                </div>
+
+
+                <!-- REVIEWS -->
+                <div v-if="activeTab === 'Reviews'" class="mt-12">
+                    REVIEWS
+                </div>
+
+
+                <!-- VIDEOS -->
+                <div v-if="activeTab === 'Video'" class="mt-12">
+                    VIDEOS
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script setup lang="ts">
+import { Icon } from '@iconify/vue';
+
 import BaseButton from '@/UI/BaseButton.vue';
 import BaseBadge from '@/UI/BaseBadge.vue';
 import { IProduct } from '../../types';
 import PriceTag from '../PriceTag.vue';
+import { ref } from 'vue';
+
+type Tabs = 'Description' | 'Additional info' | 'Reviews' | 'Video';
+const tabs: Tabs[] = ['Description', 'Additional info', 'Reviews', 'Video'];
 
 defineProps<{ product: IProduct }>()
+
+const activeTab = ref<Tabs>('Description');
 </script>
