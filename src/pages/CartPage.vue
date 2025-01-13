5cfda2dd74ea65dc05a46eb0826a2e137c9fe43c
jemil-c-137 <jemil1931@gmail.com>
2025-01-13 18:58:02 +0100

    Update product names in mock API and add CartPage component
    - Renamed multiple chair products in the mock API for better branding and clarity.
    - Introduced a new CartPage component with a loading state and basic layout for cart totals.
    - Updated router configuration to include a route for the new CartPage, enabling navigation to the cart section.

---
+++ src/pages/CartPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <main>
+        <div v-if="loading" class="min-h-screen flex items-center justify-center">
+            <LoadingSpinner />
+        </div>
+        <div v-else>
+            <div class="content-container grid grid-cols-8">
+                <div class="col-span-5 h-[300px] bg-black"></div>
+                <div class="col-start-6 col-span-8 h-[300px]">
+                    <p class="text-center font-josefin text-xl">Cart totals</p>
+                    <div>
+                        <div>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import LoadingSpinner from '@/UI/LoadingSpinner.vue';
+
+const loading = ref<boolean>(false);
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
-import ProductPage from '@/pages/ProductPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,15 +12,18 @@ const router = createRouter({
     {
       path: '/product/:id',
       name: 'product-details',
-      component: ProductPage
+      component: () => import('@/pages/ProductPage.vue')
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('@/pages/AboutPage.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+
+      component: () => import('@/pages/CartPage.vue')
     }
   ]
 })
