39c1a45303748109a56bcdbee2bbaf1de0fb44f5
jemil-c-137 <jemil1931@gmail.com>
2025-01-14 23:26:51 +0100

    Enhance CartPage layout and styling
    - Improved the layout of the CartPage component by adding a gap between grid items for better spacing.
    - Updated the cart totals section with enhanced styling, including background color, padding, and improved text formatting for better readability.
    - Added a new BaseButton component for the "Proceed to Checkout" action, enhancing user interaction.
    - Included a message indicating that shipping and taxes are calculated at checkout, providing clarity to users.

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -4,15 +4,29 @@
             <LoadingSpinner />
         </div>
         <div v-else>
-            <div class="content-container grid grid-cols-8">
+            <div class="content-container grid grid-cols-8 gap-8">
                 <div class="col-span-5 h-[300px] bg-black"></div>
                 <div class="col-start-6 col-span-8 h-[300px]">
-                    <p class="text-center font-josefin text-xl">Cart totals</p>
-                    <div>
-                        <div>
-                            <span></span>
+                    <p class="text-center font-josefin text-xl mb-6">Cart totals</p>
+                    <div class="bg-brand-bg-gray p-6">
+                        <div class="mb-4 flex justify-between relative after:absolute after:h-[2px] after:bg-brand-decorative after:top-6 after:w-full">
+                            <span class="text-md font-semibold">Subtotals</span>
+                            <span>$200</span>
                         </div>
+
+                        <div class="mb-6 flex justify-between relative after:absolute after:h-[2px] after:bg-brand-decorative after:top-6 after:w-full">
+                            <span class="text-md font-semibold">Totals</span>
+                            <span>$350</span>
+                        </div>
+
+                        <p class="mb-6 text-xs text-brand-muted">✅ Shipping & taxes calculated at checkout</p>
+
+                        <BaseButton :style="'secondary'">
+                            Proceed to Checkout
+                        </BaseButton>
                     </div>
+
+                    <p class="text-center font-josefin text-xl mb-6">Calculate Shipping</p>
                 </div>
             </div>
         </div>
@@ -22,6 +36,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import LoadingSpinner from '@/UI/LoadingSpinner.vue';
+import BaseButton from '@/UI/BaseButton.vue';
 
 const loading = ref<boolean>(false);
 </script>
